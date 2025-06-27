a8e1a91f2b1c3794399f3fdeb4d54f1aebbf0c84
ghebr <ghazal.ebrahimi@mimik.com>
2025-06-27 14:58:41 -0700

    add camera url option

--- examples/tree_demo/tree_demo.py
+++ examples/tree_demo/tree_demo.py
@@ -1,84 +1,69 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+(nanoowlenv) ghebr@tegra-ubuntu:~/Desktop/nanoowl/examples$ cd tree_demo
+(nanoowlenv) ghebr@tegra-ubuntu:~/Desktop/nanoowl/examples/tree_demo$ ls
+index.html  tree_demo.py
+(nanoowlenv) ghebr@tegra-ubuntu:~/Desktop/nanoowl/examples/tree_demo$ vim tree_demo.py
 
 
-import asyncio
-import argparse
-from aiohttp import web, WSCloseCode
-import logging
-import weakref
-import cv2
-import time
-import PIL.Image
-import matplotlib.pyplot as plt
-from typing import List
-from nanoowl.tree import Tree
-from nanoowl.tree_predictor import (
-    TreePredictor
-)
-from nanoowl.tree_drawing import draw_tree_output
-from nanoowl.owl_predictor import OwlPredictor
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("image_encode_engine", type=str)
-    parser.add_argument("--image_quality", type=int, default=50)
-    parser.add_argument("--port", type=int, default=7860)
-    parser.add_argument("--host", type=str, default="0.0.0.0")
-    parser.add_argument("--camera", type=int, default=0)
-    parser.add_argument("--resolution", type=str, default="640x480", help="Camera resolution as WIDTHxHEIGHT")
-    args = parser.parse_args()
-    width, height = map(int, args.resolution.split("x"))
-
-    CAMERA_DEVICE = args.camera
-    IMAGE_QUALITY = args.image_quality
-
-    predictor = TreePredictor(
-        owl_predictor=OwlPredictor(
-            image_encoder_engine=args.image_encode_engine
-        )
-    )
-
-    prompt_data = None
-
-    def get_colors(count: int):
-        cmap = plt.cm.get_cmap("rainbow", count)
-        colors = []
-        for i in range(count):
-            color = cmap(i)
-            color = [int(255 * value) for value in color]
-            colors.append(tuple(color))
-        return colors
 
 
-    def cv2_to_pil(image):
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return PIL.Image.fromarray(image)
 
 
-    async def handle_index_get(request: web.Request):
-        logging.info("handle_index_get")
-        return web.FileResponse("./index.html")
 
 
-    async def websocket_handler(request):
 
-        global prompt_data
 
-        ws = web.WebSocketResponse()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         await ws.prepare(request)
 
@@ -121,8 +106,10 @@ if __name__ == "__main__":
         loop = asyncio.get_running_loop()
 
         logging.info("Opening camera.")
-
-        camera = cv2.VideoCapture(CAMERA_DEVICE)
+        if CAMERA_URL:
+            camera = cv2.VideoCapture(CAMERA_URL)
+        else:
+            camera = cv2.VideoCapture(CAMERA_DEVICE)
         camera.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         camera.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
@@ -144,48 +131,3 @@ if __name__ == "__main__":
                     image_pil,
                     tree=prompt_data_local['tree'],
                     clip_text_encodings=prompt_data_local['clip_encodings'],
-                    owl_text_encodings=prompt_data_local['owl_encodings']
-                )
-                t1 = time.perf_counter_ns()
-                dt = (t1 - t0) / 1e9
-                tree = prompt_data_local['tree']
-                image = draw_tree_output(image, detections, prompt_data_local['tree'])
-
-            image_jpeg = bytes(
-                cv2.imencode(".jpg", image, [cv2.IMWRITE_JPEG_QUALITY, IMAGE_QUALITY])[1]
-            )
-
-            return re, image_jpeg
-
-        while True:
-
-            re, image = await loop.run_in_executor(None, _read_and_encode_image)
-            
-            if not re:
-                break
-            
-            for ws in app["websockets"]:
-                await ws.send_bytes(image)
-
-        camera.release()
-
-
-    async def run_detection_loop(app):
-        try:
-            task = asyncio.create_task(detection_loop(app))
-            yield
-            task.cancel()
-        except asyncio.CancelledError:
-            pass
-        finally:
-            await task
-
-
-    logging.basicConfig(level=logging.INFO)
-    app = web.Application()
-    app['websockets'] = weakref.WeakSet()
-    app.router.add_get("/", handle_index_get)
-    app.router.add_route("GET", "/ws", websocket_handler)
-    app.on_shutdown.append(on_shutdown)
-    app.cleanup_ctx.append(run_detection_loop)
-    web.run_app(app, host=args.host, port=args.port)
